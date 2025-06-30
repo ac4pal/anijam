9468739f145af08ff8a31fc863be292960476f51
ac4pal <sunbeam.mowing-4@icloud.com>
2025-06-30 21:10:19 +1000

    file paths?

--- src/app/loader.tsx
+++ src/app/loader.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import React from "react";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 
 export const LoadingState = React.memo(({fullScreen} : {fullScreen: boolean}) => {
   return <div className={fullScreen ? styles.loaderFullWidth : styles.loaderSmall}>
-    <img className={styles.pulseImage} width={200} height={101} src="/anijam.avif" alt="loading" />
+    <img className={styles.pulseImage} width={200} height={101} src={isProd ? "/anijam/anijam.avif" : "/anijam.avif"} alt="loading" />
   </div>
 })
 
